1bdd1ebc00699b41ceb04364888bdc9805023dac
aralaa <90aralala@gmail.com>
2019-08-13 22:17:41 +0900

    Revert "아라 : 소켓 테스트 커밋"
    This reverts commit 39d4630fd4911a90dab93a9be36933163f5d5b57.

--- AllBankBot2019-src/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=allbankbot2019-Web-Deploy.pubxml -p:Password=a8Z4mahy0tTS7QxkcJLoj9ex67wMZxRq2khR4Jb5ks6FmoK2kAxAo9zNAcNb
-
